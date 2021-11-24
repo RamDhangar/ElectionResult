a3208e8b48fea56df6ea8f523af08c27bebd826b
RamDhangar <93071175+RamDhangar@users.noreply.github.com>
2021-11-24 12:14:20 +0530

    Add files via upload

---
+++ vote.js
@@ -0,0 +1,52 @@
+let bjp=document.getElementById("bjp");
+let congress=document.getElementById("congrass");
+let aap=document.getElementById("aap");
+let bjpResult=0;
+let congressResult=0;
+let appResult=0;
+bjp.addEventListener("click", function(e){
+    let bool=confirm("Are sure you wnat to vote for BJP")
+    if(bool){
+    bjpResult++;
+    }
+    console.log(bjpResult);
+});
+congress.addEventListener("click", function(){
+    let bool=confirm("Are sure you wnat to vote for BJP")
+    if(bool)
+    congressResult++;
+    
+   console.log(congressResult);
+   });
+aap.addEventListener("click", function(){
+    let bool=confirm("Are sure you wnat to vote for BJP")
+    if(bool)
+    appResult++;
+    
+   console.log(appResult)
+   });
+
+   let largest;
+
+   
+    
+let showResult=document.getElementById("showResult");
+showResult.addEventListener("click",function(){
+    document.getElementById("bjpre").innerHTML='BJP Vote is <br><h2>'+bjpResult+'</h2>';
+    document.getElementById("congrassre").innerHTML='Congress Vote is <br><h2> '+congressResult+'</h2>';
+    document.getElementById("aapre").innerHTML='AAP Vote is<br> <h2>'+appResult+'</h2>';
+    let sr= document.getElementById("winResult");
+    if(bjpResult==congressResult==appResult){
+        sr.innerHTML='No one win Election';
+    }
+    else if(bjpResult>= congressResult && bjpResult >= appResult) {
+        sr.innerHTML='BJP win The Election';
+      }
+    else if (congressResult >= bjpResult && congressResult >= appResult) {
+        sr.innerHTML='Congress win The Election';
+      }
+    else {
+        sr.innerHTML='AAP win The Election';
+      }
+    
+})
